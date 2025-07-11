7df2026dd89dba0b9fff089dc4be7eb81ce2e6b8
Marcel Planas <marcel@iestrategic.com>
2025-07-11 11:41:15 +0200

    remove: removed deploy link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,23 +12,6 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 w-full items-center sm:items-start">
         <Header />
         <Map />
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-        </div>
       </main>
     </div>
   );
